3ac245b618bf540c46344082f2f42fbf78d5fdec
huulong <n.huu.long@gmail.com>
2021-02-21 22:09:17 +0100

    [DEV] Added Dev Menu script to Canvas Dev, with button to Exit

---
+++ Assets/Scripts/Menu/DevMenu.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DevMenu : MonoBehaviour
+{
+    [Header("References")]
+    
+    [Tooltip("Exit button")]
+    public Button buttonExit;
+
+
+    private void Awake()
+    {
+        buttonExit.onClick.AddListener(ExitGame);
+    }
+
+    private void OnDestroy()
+    {
+        if (buttonExit)
+        {
+            buttonExit.onClick.RemoveListener(ExitGame);
+        }
+    }
+
+    private void ExitGame()
+    {
+        Application.Quit();
+    }
+}
