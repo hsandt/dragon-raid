71351aebe6c2e3a6e111f5fe0ddb351b81ada3d1
huulong <n.huu.long@gmail.com>
2021-03-15 01:05:39 +0100

    [BACKGROUND] Completed parallax looping by remembering which of the original and duplicate is on the left or right, and when the right one reaches the center of the screen, warp the left one to its right

--- Assets/Scripts/InGame/Background/Background.cs
+++ Assets/Scripts/InGame/Background/Background.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.Collections;
 using UnityEngine;
+
+using CommonsHelper;
 
 /// Handles background parallax scrolling
 public class Background : MonoBehaviour
@@ -21,7 +23,13 @@ public class Background : MonoBehaviour
     /// Array of duplicates of rigidbodies of parallax layers, from farthest to closest, swapped with the original ones
     /// for continuous loop
     private Rigidbody2D[] m_DuplicateParallaxLayerRigidbodies;
-    
+
+    /// Array of references to the current left and right parallax layers, for each level.
+    /// All left layers are the original ones on start, but they swap with their duplicate every full scrolling period.
+    /// However, this period depends on the parallax level, so we must track them independently for each level.
+    /// Note that C# System.Tuple is immutable, so we use our custom Pair struct instead.
+    private Pair<Rigidbody2D, Rigidbody2D>[] leftAndRightParallaxLayerRigidbodies;
+
     
     void Start()
     {
@@ -29,15 +37,20 @@ public class Background : MonoBehaviour
         SetupAllParallaxLayersVelocity();
     }
     
-    
     private void InstantiateDuplicateParallaxLayers()
     {
-        // Create a duplicate for each parallax layer so we can have them loop continuously by swapping 
-        // the left and right copies when the left one has completely exited the screen to the left.
-        // We iterate forward so the rendering layer order is the same as the original: farthest, then closest layers.
+        // Create duplicate array to fill
         m_DuplicateParallaxLayerRigidbodies = new Rigidbody2D[parallaxLayerRigidbodies.Length];
+        
+        // Construct array of pairs too
+        leftAndRightParallaxLayerRigidbodies = new Pair<Rigidbody2D, Rigidbody2D>[parallaxLayerRigidbodies.Length];
+        
         for (int i = 0; i < parallaxLayerRigidbodies.Length; ++i)
         {
+            // Create a duplicate for this parallax layer so we can have it loop continuously by swapping 
+            // the left and right copies when the left one has completely exited the screen to the left.
+            // We iterate forward so the rendering layer order is the same as the original: farthest, then closest layers.
+            
             // Place the duplicate under Background, just on the right of the original, i.e. 1 screen width to the right
             // of the original (which is at the origin). Note that for more variety we may want to avoid
             // looping over one screen width. If so, just make wider parallax layers and chain them with
@@ -45,6 +58,10 @@ public class Background : MonoBehaviour
             Vector2 duplicatePosition = skySpriteRenderer.size.x * Vector2.right;
             m_DuplicateParallaxLayerRigidbodies[i] = Instantiate(parallaxLayerRigidbodies[i], duplicatePosition,
                 Quaternion.identity, transform);
+
+            // original parallax layer starts on the left, duplicate on the right
+            leftAndRightParallaxLayerRigidbodies[i].First = parallaxLayerRigidbodies[i];
+            leftAndRightParallaxLayerRigidbodies[i].Second = m_DuplicateParallaxLayerRigidbodies[i];
         }
     }
 
@@ -58,6 +75,32 @@ public class Background : MonoBehaviour
             // Player character is advancing to the right, so the background scrolls to the left
             parallaxLayerRigidbody2D.velocity = (parallaxSpeedBase + i * parallaxSpeedIncrement) * Vector2.left;
             duplicateParallaxLayerRigidbody2D.velocity = (parallaxSpeedBase + i * parallaxSpeedIncrement) * Vector2.left;
+        }
+    }
+
+    private void Update()
+    {
+        // Check for each left parallax layer exiting screen to the left, which is equivalent to right parallax
+        // layer entering and covering the screen completely, which is equivalent to its position crossing 0 toward left
+        for (int i = 0; i < parallaxLayerRigidbodies.Length; ++i)
+        {
+            Rigidbody2D leftParallaxLayerRigidbody2D = leftAndRightParallaxLayerRigidbodies[i].First;
+            Rigidbody2D rightParallaxLayerRigidbody2D = leftAndRightParallaxLayerRigidbodies[i].Second;
+
+            if (rightParallaxLayerRigidbody2D.position.x <= 0f)
+            {
+                // the duplicate parallax layer stops covering the right part of the screen, warp the original layer
+                // there to loop
+                // there may be a small accumulation of floating errors, it's hard to tell
+                // if you notice the two layers getting far from each other, or overlapping each other
+                // after a long play time, prefer a solution that sets position relatively to the other layer
+                leftParallaxLayerRigidbody2D.position += 2 * skySpriteRenderer.size.x * Vector2.right;
+                
+                // swap both roles now, we start another scrolling loop for this layer level
+                Rigidbody2D tempParallaxLayerRigidbody = leftAndRightParallaxLayerRigidbodies[i].First;
+                leftAndRightParallaxLayerRigidbodies[i].First = leftAndRightParallaxLayerRigidbodies[i].Second;
+                leftAndRightParallaxLayerRigidbodies[i].Second = tempParallaxLayerRigidbody;
+            }
         }
     }
 }
