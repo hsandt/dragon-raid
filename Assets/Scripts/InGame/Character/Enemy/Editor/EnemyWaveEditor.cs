4be64ce66bb9f42cae5c638cf9295346444b5541
huulong <n.huu.long@gmail.com>
2021-04-24 21:01:13 +0200

    [EDITOR] Clean Enemy Wave Editor to just show some label, no custom fields for now

--- Assets/Scripts/InGame/Character/Enemy/Editor/EnemyWaveEditor.cs
+++ Assets/Scripts/InGame/Character/Enemy/Editor/EnemyWaveEditor.cs
@@ -14,26 +14,19 @@ public class EnemyWaveEditor : Editor
         // Each editor window contains a root VisualElement object
         m_RootElement = new VisualElement();
         
+        // A stylesheet can be added to a VisualElement.
+        // The style will be applied to the VisualElement and all of its children.
+        var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/Scripts/InGame/Character/Enemy/Editor/EnemyWaveEditor.uss");
+        m_RootElement.styleSheets.Add(styleSheet);
+        
         Editor editor = CreateEditor(target);
         IMGUIContainer inspectorIMGUI = new IMGUIContainer(() => { editor.OnInspectorGUI(); });
         m_RootElement.Add(inspectorIMGUI);
-
-        // VisualElements objects can contain other VisualElement following a tree hierarchy.
-        VisualElement label = new Label("Hello World! From C#");
-        m_RootElement.Add(label);
 
         // Import UXML
         var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Scripts/InGame/Character/Enemy/Editor/EnemyWaveEditor.uxml");
         VisualElement labelFromUXML = visualTree.Instantiate();
         m_RootElement.Add(labelFromUXML);
-
-        // A stylesheet can be added to a VisualElement.
-        // The style will be applied to the VisualElement and all of its children.
-        var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/Scripts/InGame/Character/Enemy/Editor/EnemyWaveEditor.uss");
-        VisualElement labelWithStyle = new Label("Hello World! With Style");
-        labelWithStyle.styleSheets.Add(styleSheet);
-        m_RootElement.Add(labelWithStyle);
-
 
         return m_RootElement;
     }
