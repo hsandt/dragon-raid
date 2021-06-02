78506538de54411ae6841d9fec86b28c87f9691e
huulong <n.huu.long@gmail.com>
2021-06-02 20:09:44 +0200

    [DEV MENU] Update Level Text based on current level

--- Assets/Scripts/Menu/DevMenu.cs
+++ Assets/Scripts/Menu/DevMenu.cs
@@ -1,19 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class DevMenu : MonoBehaviour
 {
-    [Header("Scene references")]
+    [Header("Child references")]
+
+    [Tooltip("Level Text")]
+    public TextMeshProUGUI m_LevelTMPWidget;
     
-    [Tooltip("Exit button")]
+    [Tooltip("Exit Button")]
     public Button buttonExit;
 
 
     private void Awake()
     {
         buttonExit.onClick.AddListener(ExitGame);
+    }
+
+    private void Start()
+    {
+        m_LevelTMPWidget.text = $"Level {InGameManager.Instance.LevelData.levelIndex:00}";
     }
 
     private void OnDestroy()
