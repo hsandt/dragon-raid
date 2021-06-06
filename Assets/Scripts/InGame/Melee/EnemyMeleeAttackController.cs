e7ecd052c0e59d75869819c602b077992ad78b5c
huulong <n.huu.long@gmail.com>
2021-06-06 17:58:33 +0200

    [MELEE] Added Enemy Melee Attack Controller, and rearranged SEO

---
+++ Assets/Scripts/InGame/Melee/EnemyMeleeAttackController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+/// System for MeleeAttackIntention on Enemy character: handles control
+/// SEO: before MeleeAttack
+public class EnemyMeleeAttackController : BaseMeleeAttackController
+{
+    private void FixedUpdate()
+    {
+        // Put AI logic here
+    }
+}
