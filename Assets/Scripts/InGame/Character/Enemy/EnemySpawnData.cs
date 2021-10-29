ea9d1ed57a3ae339eda15ac54cd39f4e5fcfbc26
huulong <n.huu.long@gmail.com>
2021-10-29 21:23:13 +0200

    [AI] Removed old overrideRootAction from EnemySpawnData, and updated all wave objects in all Levels

--- Assets/Scripts/InGame/Character/Enemy/EnemySpawnData.cs
+++ Assets/Scripts/InGame/Character/Enemy/EnemySpawnData.cs
@@ -18,8 +18,8 @@ public class EnemySpawnData
              "If chaining multiple enemies at the same position, prefer Enemy Chain Spawn Data.")]
     public float delay = 0f;
 
-    [Tooltip("Optional action sequence used to override the default one on this specific enemy. " +
-             "Useful to customize enemy behavior, by either only changing parameters or the full action sequence.")]
-    public ActionSequence overrideActionSequence = null;
+    [Tooltip("Optional root action used to override the default one on this specific enemy. " +
+             "It actually represents the whole Behaviour Tree made by this root and all its children ." +
+             "Useful to customize enemy behavior, by either only changing parameters or the full behaviour tree.")]
     public BehaviourAction overrideRootAction = null;
 }
