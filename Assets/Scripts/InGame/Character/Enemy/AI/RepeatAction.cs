ed039fbc0e5d0d8d336c1923844679488abf7227
huulong <n.huu.long@gmail.com>
2022-01-26 21:24:13 +0100

    [ACTION] Added Repeat Count parameter to RepeatAction. 0 for infinite repeat (as previously)

--- Assets/Scripts/InGame/Character/Enemy/AI/RepeatAction.cs
+++ Assets/Scripts/InGame/Character/Enemy/AI/RepeatAction.cs
@@ -4,28 +4,39 @@ using UnityEngine;
 
 using CommonsHelper;
 
+/// Action to repeat the decorated action N times (or indefinitely)
 public class RepeatAction : BehaviourAction
 {
+    [Header("Parameters")]
+
+    [SerializeField, Tooltip("Number of times to repeat the decorated action. If 0, repeat forever.")]
+    [Min(0)]
+    private int repeatCount = 0;
+
+
     /* Cached child references */
-    
-    /// Behaviour action on child (only single child supported, as Repeat is a decorator node
+
+    /// Behaviour action on child (only single child supported, as Repeat is a decorator node)
     private BehaviourAction m_RepeatedAction;
 
-    
+
     /* State vars */
 
     /// True iff child is not running yet, or has finished execution this frame
-    /// (Repeat never stops, so it will re-run the child right on next frame)
+    /// (if there are still repetitions to do, we will re-run the child right on next frame)
     private bool m_IsChildRunning;
 
-    
+    /// Number of times we have completed the decorated action
+    private int m_CompletedRunCount;
+
+
     private void Awake()
     {
         int childCount = transform.childCount;
         if (childCount > 0)
         {
             m_RepeatedAction = transform.GetChild(0).GetComponentOrFail<BehaviourAction>();
-            
+
             #if UNITY_EDITOR || DEVELOPMENT_BUILD
             Debug.AssertFormat(childCount == 1, this,
                 "[RepeatAction] There are {0} children, expected 1. Only the first child will be repeated.",
@@ -43,29 +54,37 @@ public class RepeatAction : BehaviourAction
     public override void OnStart()
     {
         m_IsChildRunning = false;
+        m_CompletedRunCount = 0;
     }
 
     public override void RunUpdate()
     {
         // Pattern is similar to RunActionSequence, but much simpler as run on a single action
-        // and never ending
         // However we must still temporize the calls, waiting for next frame if the action is over,
-        // to avoid getting stuck in an infinite loop in case it's gonna be over forever.
-        
+        // to avoid getting stuck in an infinite loop in case the child action is gonna be over forever.
+
+        // No need to check for m_CompletedRunCount vs repeatCount here,
+        // as we assume we've checked IsOver before calling this
         if (!m_IsChildRunning)
         {
             m_RepeatedAction.OnStart();
             m_IsChildRunning = true;
         }
-        
+
         if (m_RepeatedAction.IsOverOrDeactivated())
         {
             // Call OnEnd to cleanup anything set by the m_RepeatedAction we don't want anymore
             m_RepeatedAction.OnEnd();
 
-            // m_RepeatedAction is over, so stop RepeatAction for this frame
-            // (but we'll restart it next frame)
+            // m_RepeatedAction is over, so flag action as stopped for this frame
+            // (if we have still runs to do, we'll restart the action next frame)
             m_IsChildRunning = false;
+
+            // Increment the run count if it matters
+            if (repeatCount > 0)
+            {
+                ++m_CompletedRunCount;
+            }
         }
 
         if (m_IsChildRunning)
@@ -76,7 +95,8 @@ public class RepeatAction : BehaviourAction
 
     protected override bool IsOver()
     {
-        // Repeat never ends
-        return false;
+        // If repeat count is 0, never end
+        // Else, end after the wanted number of repetitions
+        return repeatCount > 0 && m_CompletedRunCount >= repeatCount;
     }
 }
